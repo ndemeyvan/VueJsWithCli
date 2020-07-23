86aada461b2200db2890636472d86d74893b2435
Ndeme <y.ndeme@sevendadvancedacademy.com>
2020-07-23 18:34:49 +0100

    meilleur structuration du project

---
+++ vue/src/components/Server/Servers.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="col-xs-12 col-sm-6">
+        <ul class="list-group">
+            <li class="list-group-item" v-for="(item) in 5" :key="item">
+                Server #{{ item }}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+    div {
+        border: 1px solid blue;
+    }
+</style>
--- vue/src/main.js
+++ vue/src/main.js
@@ -1,9 +1,5 @@
 import Vue from 'vue'
 import App from './App.vue'
-import Home from './Home.vue';
-
-//ceci est une importation globale du component
-Vue.component('app-server',Home);
 
 new Vue({
   el: '#app',
