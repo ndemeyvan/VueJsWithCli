0de4c6ee9c72e0dcaadf7f4fe52825c16e07f07f
Ndeme <y.ndeme@sevendadvancedacademy.com>
2020-07-23 17:44:10 +0100

    bonne initiation au component

---
+++ vue/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+
+   <app-server></app-server>
+
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+</style>
---
+++ vue/src/ServerStatus.vue
@@ -0,0 +1,37 @@
+<template>
+
+
+  <div>
+        <p>Serve status: {{ status }}</p>
+        <button @click="changeStatus">Change serve status</button>
+  </div>
+
+
+</template>
+
+
+
+
+
+
+<script>
+export default {
+  data: function() {
+    return {
+      status: "Critical"
+    };
+  },
+  methods: {
+    changeStatus() {
+        this.status = " Normal";
+    }
+  }
+};
+</script>
+
+
+
+
+<style>
+
+</style>
