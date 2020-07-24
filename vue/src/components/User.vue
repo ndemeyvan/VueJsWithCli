b9f4bab50e8c744cbacd48c09e982b2c02859c8b
Ndeme <y.ndeme@sevendadvancedacademy.com>
2020-07-24 12:30:36 +0100

    communications entres les components parent et enfants

---
+++ vue/src/components/User.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="component">
+    <h1>The User Component</h1>
+    <p>I'm an awesome User!</p>
+    <button @click="changeInChildComponent">
+      Change my name
+    </button>
+    <hr />
+    <div class="row">
+      <div class="col-xs-12 col-sm-6">
+        <app-user-detail :myName="name" v-on:nameIsReset="name = $event"></app-user-detail>
+      </div>
+      <div class="col-xs-12 col-sm-6">
+        <app-user-edit></app-user-edit>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import UserDetail from "./UserDetail.vue";
+import UserEdit from "./UserEdit.vue";
+
+export default {
+    data() {
+        return {
+            name : "Yvana",
+        }
+    },
+  components: {
+    "app-user-detail": UserDetail,
+    "app-user-edit": UserEdit
+  },
+  methods: {
+    changeInChildComponent: function() {
+        this.name= "Micheal Nde";    }
+  }
+};
+</script>
+
+<style scoped>
+div {
+  background-color: lightblue;
+}
+</style>
---
+++ vue/src/components/UserDetail.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="component">
+    <h3>You may view the User Details here</h3>
+    <p>Many Details</p>
+    <p>User Name is : {{  
+        // swithCaracter()
+        myName
+         }}</p>
+    <button @click="resetName">Reset the name</button>
+  </div>
+</template>
+
+<script>
+export default {
+    props: {
+        myName:{
+            type:String,
+            required:true,
+        }
+    },
+    methods: {
+
+        swithCaracter:function(){
+                return this.myName.split('').reverse().join("");
+        },
+        resetName:function() {
+
+            this.myName = "Max";
+            this.$emit('nameIsReset',this.myName);
+            
+        }
+        
+    },
+};
+</script>
+
+<style scoped>
+div {
+  background-color: lightcoral;
+}
+</style>
