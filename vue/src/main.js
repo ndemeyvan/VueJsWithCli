921ee990736a0856d11c00ce4c44052e18b91878
Ndeme <y.ndeme@sevendadvancedacademy.com>
2020-07-24 22:57:42 +0100

    Comment cree sa propre directive avec vuejs

--- vue/src/main.js
+++ vue/src/main.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+Vue.directive('highLight',{
+    bind(el,bindind,vnode){
+      el.style.backgroundColor = 'green';
+    }
+})
 new Vue({
   el: '#app',
   render: h => h(App)
