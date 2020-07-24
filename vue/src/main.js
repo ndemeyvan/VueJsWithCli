fbad125a3aff5689636c7ea48190270695afe2c4
Ndeme <y.ndeme@sevendadvancedacademy.com>
2020-07-24 23:58:20 +0100

    itilisation des filtres en Vuejs equivalent des pipa

--- vue/src/main.js
+++ vue/src/main.js
@@ -1,11 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-Vue.directive('highLight',{
-    bind(el,bindind,vnode){
-      el.style.backgroundColor = 'green';
-    }
-})
 new Vue({
   el: '#app',
   render: h => h(App)
